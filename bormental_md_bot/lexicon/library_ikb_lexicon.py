3b50b90eeec16dc4c6153fc9eab2b3eb0f98b625
harlok <holmik90@yandex.ru>
2023-04-10 12:28:03 +0400

    feat(./library_ikb_lexicon.py): add inline kb lexicon

---
+++ bormental_md_bot/lexicon/library_ikb_lexicon.py
@@ -0,0 +1,84 @@
+from typing import Dict, List, Tuple, Union
+
+GET_PREV_DATA: str = 'cat_choice_menu'
+TO_MENU_DATA: str = 'cat_choice_menu'
+GO_DB: str = 'db_cat'
+GO_PYTHON: str = 'python_cat'
+
+# a tuple with numbers controls the position of the buttons in the row
+MENU_IKB: Dict[str, List[Union[Tuple[str, str], Tuple[int]]]] = {
+    'cat_choice_menu':
+        [('PYTHON', 'python_cat'),
+         ('БАЗЫ ДАННЫХ', 'db_cat'),
+         ('GIT', 'git_cat'),
+         ('LINUX', 'linux_cat'),
+         ('АЛГОРИТМЫ', 'algorithms_cat'),
+         ('КНИГИ ДЛЯ СТАРТА', 'start_cat'),
+         ('РЕКОМЕНДАЦИИ', 'recommendations_cat'),
+         ('ЗАКРЫТЬ МЕНЮ', 'close'),
+         (2, 2, 2, 1)],
+
+    'db_cat':
+        [('SQL', 'sql_cat'),
+         ('NoSQL', 'nosql_cat'),
+         # ('Подробнее об SQLHelper', 'sql_helper'),
+         ('НАЗАД', GET_PREV_DATA),
+         (2, 1)],
+
+    'python_cat':
+        [('КНИГИ ПО PYTHON', 'pybook_cat'),
+         ('ДЛЯ САМЫХ МАЛЕНЬКИХ', 'kids_cat'),
+         ('АСИНХРОННЫЙ PYTHON', 'async_cat'),
+         ('DJANGO', 'django_cat'),
+         ('ТЕСТИРОВАНИЕ', 'tests_cat'),
+         ('PANDAS', 'pandas_cat'),
+         ('НЕЙРОСЕТИ', 'ml_cat'),
+         ('НАЗАД', GET_PREV_DATA),
+         (1,)],
+}
+
+BOOK_IKB: Dict[str, str] = {
+    'sql_cat': GO_DB,
+    'nosql_cat': GO_DB,
+    'pybook_cat': GO_PYTHON,
+    'kids_cat': GO_PYTHON,
+    'async_cat': GO_PYTHON,
+    'django_cat': GO_PYTHON,
+    'tests_cat': GO_PYTHON,
+    'pandas_cat': GO_PYTHON,
+    'ml_cat': GO_PYTHON,
+    'linux_cat': GET_PREV_DATA,
+    'algorithms_cat': GET_PREV_DATA,
+    'git_cat': GET_PREV_DATA,
+    'start_cat': GET_PREV_DATA,
+    'recommendations_cat': GET_PREV_DATA
+}
+
+TY_BUTTONS: Dict[str, Tuple[str, ...]] = {
+    'ty_cmd': ('СПАСИБО!',
+               'ПОНЯТНО!',
+               'ХОРОШО!',
+               'БЛАГОДАРЮ!'),
+    'praise_cmd': (
+        'Есть над чем подумать...',
+        'Сколько же всего ты знаешь!',
+        'Это очень интересно🤔')
+}
+
+# cb data must be named the same as the fields in the database
+BOOK_INFO_BUT = (
+    ('ПОДРОБНЕЕ О КНИГЕ', 'description'),
+    ('ОТЗЫВЫ ДЕДОВ', 'reviews'),
+    ('СОДЕРЖАНИЕ', 'content'),
+    (1,)
+)
+
+GO_MAIN_MENU: Dict[str, str] = {
+    'title': 'ГЛАВНОЕ МЕНЮ',
+    'cb_data': TO_MENU_DATA
+}
+
+PAGINATION_BUT: Dict[str, str] = {
+    'backward': '<<',
+    'forward': '>>'
+}
